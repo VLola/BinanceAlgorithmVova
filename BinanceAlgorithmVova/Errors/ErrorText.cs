944eda691c0d37feb3df74c14f7081f6b4529443
Valentin <valiklolanew@gmail.com>
2022-05-12 17:23:07 +0300

    Добавьте файлы проекта.

---
+++ BinanceAlgorithmVova/Errors/ErrorText.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinanceAlgorithmVova.Errors
+{
+    public static class ErrorText
+    {
+        const string patch = "error-log.txt";
+        public static void Add(string error)
+        {
+            string json = DateTime.Now.ToString() + " - " + error;
+            File.AppendAllLines(@Patch(), json.Split('\n'));
+        }
+        public static string Patch()
+        {
+            return patch;
+        }
+        public static string Directory()
+        {
+            return System.IO.Path.Combine(Environment.CurrentDirectory, "");
+        }
+        public static string FullPatch()
+        {
+            return Directory() + "/" + Patch();
+        }
+    }
+}
---
+++ BinanceAlgorithmVova/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using BinanceAlgorithmVova.Errors;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BinanceAlgorithmVova
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            ErrorWatcher();
+        }
+
+
+        #region - Error -
+        // ------------------------------------------------------- Start Error Text Block --------------------------------------
+        private void ErrorWatcher()
+        {
+            try
+            {
+                FileSystemWatcher error_watcher = new FileSystemWatcher();
+                error_watcher.Path = ErrorText.Directory();
+                error_watcher.NotifyFilter = NotifyFilters.LastWrite | NotifyFilters.LastAccess | NotifyFilters.FileName | NotifyFilters.DirectoryName;
+                error_watcher.Changed += new FileSystemEventHandler(OnChanged);
+                error_watcher.Filter = ErrorText.Patch();
+                error_watcher.EnableRaisingEvents = true;
+            }
+            catch (Exception e)
+            {
+                ErrorText.Add($"ErrorWatcher {e.Message}");
+            }
+        }
+        private void OnChanged(object source, FileSystemEventArgs e)
+        {
+            Dispatcher.Invoke(new Action(() => { ERROR_LOG.Text = File.ReadAllText(ErrorText.FullPatch()); }));
+        }
+        private void Button_ClearErrors(object sender, RoutedEventArgs e)
+        {
+            File.WriteAllText(ErrorText.FullPatch(), "");
+        }
+        // ------------------------------------------------------- End Error Text Block ----------------------------------------
+        #endregion
+    }
+}
